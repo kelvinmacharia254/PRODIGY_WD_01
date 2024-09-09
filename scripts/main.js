bb2bf417f2df1248cd44ac27f6d10b26ed50d153
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-09-09 03:31:18 +0300

    add responsive nav menu, mobile fast approach

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// GUI elements
+const smMenuSideBar = document.querySelector(".sm-menu");
+
+
+function showSideBar(){
+    smMenuSideBar.style.display = "flex";
+}
+
+
+function hideSideBar(){
+    smMenuSideBar.style.display = "none";
+}
