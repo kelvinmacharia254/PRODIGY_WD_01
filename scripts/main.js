d102cac1cc81e4b1f4909c4fc295d02a691d410d
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-09-10 08:58:30 +0300

    use event listeners approach instead of onclick to open and close sidebar nav menu for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,15 @@
-// load home page
+// load home page on initial load
 window.addEventListener('load', () => {
     window.location.hash = ('#home');
 })
 
 // GUI elements
 const smMenuSideBar = document.querySelector(".sm-menu");
+const toggleButton = document.querySelector("#toggle-button");
+const closeSideBarButton = document.querySelector("#close-side-bar");
 
 
-function showSideBar(){
-    smMenuSideBar.style.display = "flex";
-}
-
-
-function hideSideBar(){
-    smMenuSideBar.style.display = "none";
-}
+// open sidebar nav menu
+toggleButton.addEventListener("click", () => smMenuSideBar.style.display = "flex");
+// close sider nav menu
+closeSideBarButton.addEventListener("click", () => smMenuSideBar.style.display = "none");
